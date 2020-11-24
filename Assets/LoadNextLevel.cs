74547c3efa266a55b013c1278687d75a72034490
glaverghetta1 <72049739+glaverghetta1@users.noreply.github.com>
2020-11-23 20:34:26 -0500

    Added files

---
+++ Assets/LoadNextLevel.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadNextLevel : MonoBehaviour
+{
+    public void NextLevel()
+    {
+        SceneManager.LoadScene(2);
+    }
+}
---
+++ Assets/beginGame.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class beginGame : MonoBehaviour
+{
+    public void begin()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
