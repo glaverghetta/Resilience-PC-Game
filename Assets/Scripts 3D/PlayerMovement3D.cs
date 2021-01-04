fb7db3c35084da56e7de08b152c518eaef979c26
glaverghetta1 <72049739+glaverghetta1@users.noreply.github.com>
2021-01-04 12:28:16 -0500

    3D Test
    Created a scene to test first-person 3D environments.

---
+++ Assets/Scripts 3D/PlayerMovement3D.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement3D : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float speed = 12f;
+    public float gravity = -9.81f;
+    public float jumpHeight = 3f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    public Transform ceilingCheck;
+    public float ceilingDistance = 0.4f;
+    public LayerMask ceilingMask;
+
+    Vector3 velocity;
+    bool isGrounded;
+    bool hitCeiling;
+
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        hitCeiling = Physics.CheckSphere(ceilingCheck.position, ceilingDistance, ceilingMask);
+        if (hitCeiling && velocity.y > 0)
+        {
+            velocity.y -= velocity.y;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        if(Input.GetButtonDown("Jump") && isGrounded)   //jumping
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime); //gravity
+    }
+}
