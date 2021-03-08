3898bd48ddd8f10f24984fc82b973f8b29aafc8f
glaverghetta1 <72049739+glaverghetta1@users.noreply.github.com>
2021-03-08 17:57:39 -0500

    Beginnings of Star Ratings
    Revamped the intro screen; added more instructions and also information about Star Ratings (the ratings themselves have not been implemented yet).

--- Assets/Scripts/beginGame.cs
+++ Assets/Scripts/beginGame.cs
@@ -3,9 +3,37 @@ using UnityEngine.SceneManagement;
 
 public class beginGame : MonoBehaviour
 {
+    public GameObject welcomeScreen;
+    public GameObject instructions;
+    public GameObject starRatingText;
+
     public void begin()
     {
         SceneManager.LoadScene(1);
+    }
+
+    public void showInstructions()
+    {
+        welcomeScreen.SetActive(false);
+        instructions.SetActive(true);
+    }
+
+    public void hideInstructions()
+    {
+        welcomeScreen.SetActive(true);
+        instructions.SetActive(false);
+    }
+
+    public void showStarRatingText()
+    {
+        welcomeScreen.SetActive(false);
+        starRatingText.SetActive(true);
+    }
+
+    public void hideStarRatingText()
+    {
+        welcomeScreen.SetActive(true);
+        starRatingText.SetActive(false);
     }
 
     public void goToChangeColor()
